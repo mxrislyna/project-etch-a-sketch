9c2f60c70462dec9bc7facf876a58bb9018d05be
mxrislyna <160684475+mxrislyna@users.noreply.github.com>
2026-07-31 12:14:34 -0500

    Add function getRandomColor

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 const container = document.querySelector("#grid-container");
 
+
+//Grid creator
 function gridBuilder (squaresPerSide) {
     let totalSquares = squaresPerSide * squaresPerSide;
 
         for (let i = 0; i < totalSquares; i++) {
-                const square = document.createElement('div');
-                const squareSize = 100 / squaresPerSide;
+            const square = document.createElement('div');
+            const squareSize = 100 / squaresPerSide;
                 square.classList.add('square');
                 square.style.width = `${squareSize}%`;
                 square.style.height = `${squareSize}%`;
                 container.appendChild(square);
+
                 square.addEventListener('mouseenter', () => {
+                    square.style.backgroundColor = getRandomColor();
                     square.classList.add('colored');
                 });
             }
 }
 
+
+//UserInput prompt button
 const btn = document.querySelector("#btn");
 
 btn.addEventListener('click', () => {
@@ -29,5 +35,43 @@ btn.addEventListener('click', () => {
     }
 });
 
+
+//Reset button
+const reset = document.querySelector("#reset-btn");
+
+reset.addEventListener('click', () => {
+    const squares = document.querySelectorAll('.square');
+
+    squares.forEach(square => {
+        square.style.backgroundColor = 'beige';
+    });
+});
+
+
+//Eraser
+const eraser = document.querySelector('#eraser');
+
+let isEraserActive = false;
+
+eraser.addEventListener = ('click', () => {
+    isEraserActive = !isEraserActive;
+
+    eraser.addEventListener = ('mouseover', () => {
+       if (isEraserActive) {
+        square.style.backgroundColor = 'white';
+       } else {
+        square.style.backgroundColor = rgb(255, 0, 128) 
+       }
+    });
+});
+
+//Color randomizer
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    return `rgb(${r}, ${g}, ${b})`;
+}
 
 gridBuilder(16);
