f32f7a26b9b411fe4b216e7ac4646f46a9d8a717
mxrislyna <160684475+mxrislyna@users.noreply.github.com>
2026-07-31 11:04:41 -0500

    Add function gridBuilder
    *Add btn event listener to allow user to click button and enter
    desired number of squares per side
    * Fix the loop logic to prevent the default 256 squares from being
    hard-coded
    * Also add parameter for better logic calculation

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 const container = document.querySelector("#grid-container");
 
-for (let i = 0; i < 256; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    container.appendChild(square);
-    square.addEventListener('mouseenter', () => {
-        square.classList.add('colored');
-    });
+function gridBuilder (squaresPerSide) {
+    let totalSquares = squaresPerSide * squaresPerSide;
+
+        for (let i = 0; i < totalSquares; i++) {
+                const square = document.createElement('div');
+                const squareSize = 100 / squaresPerSide;
+                square.classList.add('square');
+                square.style.width = `${squareSize}%`;
+                square.style.height = `${squareSize}%`;
+                container.appendChild(square);
+                square.addEventListener('mouseenter', () => {
+                    square.classList.add('colored');
+                });
+            }
 }
+
+const btn = document.querySelector("#btn");
+
+btn.addEventListener('click', () => {
+    let userInput = Number(prompt("", "ex. 10"));
+    container.innerHTML = "";
+    gridBuilder(userInput);
+});
+
+gridBuilder(16);
